22712295cbe9c23b8727490cf912bdaa4b04009a
Tomasz Bawor <bawortomasz@gmail.com>
2023-05-10 21:29:17 +0200

    Initial commit

---
+++ src/main/kotlin/com/tbawor/erpe/ErpeApplication.kt
@@ -0,0 +1,11 @@
+package com.tbawor.erpe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ErpeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ErpeApplication>(*args)
+}
---
+++ src/test/kotlin/com/tbawor/erpe/ErpeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tbawor.erpe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ErpeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
